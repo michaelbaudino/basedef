f9cc1af69040e6a8b53e1031f0d7f7a5a1e7795a
Michael Baudino <michael.baudino@alpine-lab.com>
2016-03-31 08:22:11 +0400

    Switch from vanilla CSS to Sass

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :basedef, Basedef.Endpoint,
 config :basedef, Basedef.Endpoint,
   live_reload: [
     patterns: [
-      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg|sass)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
       ~r{web/templates/.*(eex|slim|slime)$}
